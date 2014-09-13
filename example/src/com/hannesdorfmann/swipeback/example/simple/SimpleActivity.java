50052a4fe840574b4ef0a02e5dc42f4404c8d77d
chenupt <chenupt@gmail.com>
2014-09-13 20:10:34 +0800

    edit title text

--- example/src/com/hannesdorfmann/swipeback/example/simple/SimpleActivity.java
+++ example/src/com/hannesdorfmann/swipeback/example/simple/SimpleActivity.java
@@ -3,7 +3,6 @@ package com.hannesdorfmann.swipeback.example.simple;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.Log;
 
 import com.hannesdorfmann.swipeback.Position;
 import com.hannesdorfmann.swipeback.SwipeBack;
@@ -18,7 +17,6 @@ public class SimpleActivity extends SwipeBackActivity {
 	@Override
 	public void onCreate(Bundle saved){
 		super.onCreate(saved);
-		// Init the swipe back mechanism
         setContentView(R.layout.activity_simple);
 
         swipeBack = SwipeBack.attach(this, Position.LEFT)
@@ -27,8 +25,8 @@ public class SimpleActivity extends SwipeBackActivity {
         swipeBack.setOnOpenListener(new SwipeBack.OnOpenListener() {
             @Override
             public boolean onOpen() {
-                AlertDialog dialog = new AlertDialog.Builder(SimpleActivity.this).setMessage("sdf")
-                        .setPositiveButton("dd", new DialogInterface.OnClickListener() {
+                AlertDialog dialog = new AlertDialog.Builder(SimpleActivity.this).setMessage("Exit?")
+                        .setPositiveButton("ok", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialogInterface, int i) {
                                 SimpleActivity.super.finish();
@@ -39,11 +37,5 @@ public class SimpleActivity extends SwipeBackActivity {
             }
         });
 	}
-
-    @Override
-    public void finish() {
-        Log.d("eeee", "finish");
-        super.finish();
-    }
 
 }
